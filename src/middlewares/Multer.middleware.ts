43d5c06e7017083ac526b603cd7d17dcad0ff5ab
Shubham Prakash <shubhamprakash681@gmail.com>
2025-04-17 23:58:12 +0530

    Refactor multer middleware to dynamically set upload directory (as Vercel server can store file only in /tmp directory) based on environment

--- src/middlewares/Multer.middleware.ts
+++ src/middlewares/Multer.middleware.ts
@@ -1,8 +1,34 @@
 import multer from "multer";
+import fs from "fs";
+import os from "os";
+import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp/my-uploads");
+    const getDestinationFolder = (): string => {
+      console.log("here, process.env: ", process.env.NODE_ENV);
+
+      // using /tmp folder of vercel VM as it has write access
+      if (process.env.NODE_ENV === "production") {
+        const tempUploadDir = path.join(os.tmpdir(), "temp", "my-uploads");
+        console.log("tempUploadDir: ", tempUploadDir);
+
+        // Ensure the temporary directory exists (handle potential errors)
+        try {
+          if (!fs.existsSync(tempUploadDir)) {
+            fs.mkdirSync(tempUploadDir, { recursive: true });
+            console.log("!!!Temporary directory created!!!");
+          }
+        } catch (err) {
+          console.error("!!!Error creating temporary directory:", err);
+        }
+
+        return tempUploadDir;
+      }
+
+      return "./public/temp/my-uploads";
+    };
+    cb(null, getDestinationFolder());
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
